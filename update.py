ce229e79a8d501882cc3f43221b7f508367552a1
Stuart <Stuart@dyn-72-33-141-38.uwnet.wisc.edu>
2014-05-13 15:16:01 -0500

    Added files

---
+++ update.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import subprocess, sys, time
+
+print("Starting Update.py")
+print(time.strftime("%b  %d %X", time.localtime()))
+agUpdate = "apt-get -q update"
+agUpgrade = "apt-get -qy upgrade"
+rpiUpdate = "rpi-update"
+reboot = "/sbin/shutdown -r +1"
+commands = [agUpdate, agUpgrade, rpiUpdate]
+
+try:
+	for cmd in commands:
+		try:
+			subprocess.call(cmd, shell=True)
+		except Exception, e:
+			print(e)
+
+finally:
+	subprocess.Popen(reboot, shell=True)
+	sys.exit()
